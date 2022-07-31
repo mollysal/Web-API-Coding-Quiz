5e1ed5bbaaa19068cc67dbd2f68500a4de4e321d
Molly Salmonsen <mollysalmonsen@Mollys-MacBook-Pro-2.local>
2022-07-31 12:28:31 -0400

    somewhat working timer & decrease for incorrect answers

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -5,11 +5,13 @@ var scoreText = document.querySelector('#score')
 var progressText = document.querySelector('#progressText')
 var timer = document.querySelector("#timer")
 
+//Defining Variables in JS 
 let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
 let questionCounter = 0
 let availableQuestions = []
+let timeLeft = 76
 
 //Questions & Answer Array will show up in question & choice-text in HTML
 let questions = [
@@ -50,6 +52,8 @@ let questions = [
 //Establishing a Score & Max amaount of question
 const SCORE_POINTS = 100
 const MAX_QUESTIONS = 4 
+//Max amount of time lost per incorrect question
+const TIME_DECREASE = 10
 
 //Establishing what is happening at the start of the game
 startGame = () => {
@@ -66,15 +70,21 @@ getNewQuestion = () => {
 
         return window.location.assign('/Users/mollysalmonsen/Documents/_Coding-Bootcamp/Homework/Web-API-Coding-Quiz/Assets/end.html')
     }
+    //Once the quiz is strarted, the timer starts counting down
+    runTimer()
 
+    //The question counter lets users know how many questions they have left out of the max number of questions, in this example 4
     questionCounter++
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
 
+    //Picking a random question from the availableQuestions array
     const questionIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionIndex]
-    /* Knowing what question to ask */
+
+    // Knowing what question to ask 
     question.innerText = currentQuestion.question
 
+    //Each correct answer is associated with a number from the avaliableQustions array
     choices.forEach(choice => {
         const number = choice.dataset['number']
         choice.innerText = currentQuestion['choice' + number]
@@ -95,10 +105,17 @@ choices.forEach(choice => {
         /* number is to the choices number of HTML */
         const selectedAnswer = selectedChoice.dataset['number']
 
+        //Depending if the choice selected is right or wrong, a class is applied from the CSS (greeen=correct)
         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
+        //if the "correct" or green class is aplied, that means the choice selected was the answer & the amount of points goes up
         if(classToApply === 'correct') {
             incrementScore(SCORE_POINTS)
+        }
+
+        if(classToApply === 'incorrect') {
+            //if the choice is wrong, the timer decreases by 10 seconds
+            runTimer(TIME_DECREASE)
         }
 
         selectedChoice.parentElement.classList.add(classToApply)
@@ -110,6 +127,19 @@ choices.forEach(choice => {
     
     })
 })
+
+function runTimer () {
+    let clock = setInterval(function() {
+        timeLeft--;
+        timer.textContent = `Time: ${timeLeft}`;
+        if(timeLeft === 0) {
+            //when the timer runs out it redirects you to the end.html where you can insert your initials for hight score
+            clearInterval(clock);
+            document.location.href = "./end.html"
+        }
+    }, 1000)
+}
+
 
 incrementScore = num => {
     score +=num
